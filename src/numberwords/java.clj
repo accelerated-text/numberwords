cb5800a2d9ac0eafcfa4e5f452513b6499c0a5dc
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-05-31 08:46:17 +0300

    feat: add Java main

--- src/numberwords/java.clj
+++ src/numberwords/java.clj
@@ -1,11 +1,17 @@
 (ns numberwords.java
-  (:require [numberwords.core :as nw]))
+  (:require [clojure.pprint :as pp]
+            [numberwords.core :as nw]))
 
 (gen-class :name ai.tokenmill.numberwords.NumberWords
-           :methods [[approximations [String Float Float] java.util.Map]])
+           :main true
+           :prefix NW-
+           :methods [[approximations [String Double Double] java.util.Map]])
 
-(defn -approximations [_ language actual-value scale]
-  (prn "Lang: " (keyword language))
-  (prn "AV: " actual-value)
-  (prn "SC: " (rationalize scale))
+(defn NW-approximations [_ language actual-value scale]
   (nw/approximations (keyword language) actual-value (rationalize scale)))
+
+(defn -main [& [language actual-value scale]]
+  (pp/pprint
+   (NW-approximations nil language
+                      (Double/parseDouble actual-value)
+                      (Double/parseDouble scale))))
